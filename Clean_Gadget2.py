f4c9953482b172e413b46bed46ad945d8854dd11
Lunar <ireena51@gmail.com>
2024-11-11 23:29:58 +0700

    Refactor Clean_Gadget.py and add Clean_Gadget2.py for enhanced JavaScript/TypeScript cleaning functionality

--- Clean_Gadget2.py
+++ Clean_Gadget2.py
@@ -1,6 +1,6 @@
 import re
 
-# JavaScript keywords; immutable set
+# JavaScript/TypeScript keywords set
 keywords = frozenset({'abstract', 'await', 'boolean', 'break', 'byte', 'case', 'catch', 'char', 'class', 'const',
                       'continue', 'debugger', 'default', 'delete', 'do', 'double', 'else', 'enum', 'export', 'extends',
                       'false', 'final', 'finally', 'float', 'for', 'function', 'goto', 'if', 'implements', 'import',
@@ -15,56 +15,72 @@ main_set = frozenset({'init', 'start'})
 # JavaScript/TypeScript doesn’t have argc/argv, but you may add similar terms as needed.
 main_args = frozenset({})
 
+# Function to clean JavaScript/TypeScript gadget
 def clean_gadget(gadget):
+    # Dictionaries to map function/variable names to symbol names
     fun_symbols = {}
     var_symbols = {}
 
     fun_count = 1
     var_count = 1
 
-    # regular expression to catch multi-line comment
+    # Regex to detect multi-line comment end
     rx_comment = re.compile(r'\*/\s*$')
-    # regular expression to find function name candidates
+    # Regex to find function names in JavaScript (e.g., `function name(...)`, `const name = (...) => {}`)
     rx_fun = re.compile(r'\b([_A-Za-z]\w*)\b(?=\s*\()')
-    # regular expression to find variable name candidates
-    rx_var = re.compile(r'\b([_A-Za-z]\w*)\b(?:(?=\s*\w+\()|(?!\s*\w+))(?!\s*\()')
+    # Regex for variable names in JavaScript (not followed by a function definition)
+    rx_var = re.compile(r'\b([_A-Za-z]\w*)\b(?!\s*\()')
 
+    # Final cleaned gadget output to return
     cleaned_gadget = []
 
     for line in gadget:
+        # Process line if it’s not a header or multi-line comment
         if rx_comment.search(line) is None:
-            # remove all string literals (keep the quotes)
-            nostrlit_line = re.sub(r'"(?:\\.|[^"\\])*"|\'(?:\\.|[^\'\\])*\'|`(?:\\.|[^`\\])*`', '""', line)
-            # replace any non-ASCII characters with empty string
-            ascii_line = re.sub(r'[^\x00-\x7f]', r'', nostrlit_line)
+            # Remove all string literals (keep the quotes)
+            nostrlit_line = re.sub(r'".*?"', '""', line)
+            # Remove character literals (JavaScript doesn't have these)
+            nocharlit_line = re.sub(r"'.*?'", "''", nostrlit_line)
+            # Remove any non-ASCII characters
+            ascii_line = re.sub(r'[^\x00-\x7f]', r'', nocharlit_line)
 
-            # return, in order, all regex matches at string list; preserves order for semantics
+            # Find function and variable names in line
             user_fun = rx_fun.findall(ascii_line)
             user_var = rx_var.findall(ascii_line)
 
+            # Map function names to unique symbols
             for fun_name in user_fun:
                 if len({fun_name}.difference(main_set)) != 0 and len({fun_name}.difference(keywords)) != 0:
-                    if fun_name not in fun_symbols.keys():
+                    if fun_name not in fun_symbols:
                         fun_symbols[fun_name] = 'FUN' + str(fun_count)
                         fun_count += 1
+                    # Replace function names (only when followed by `(`)
                     ascii_line = re.sub(r'\b(' + fun_name + r')\b(?=\s*\()', fun_symbols[fun_name], ascii_line)
 
+            # Map variable names to unique symbols
             for var_name in user_var:
                 if len({var_name}.difference(keywords)) != 0 and len({var_name}.difference(main_args)) != 0:
-                    if var_name not in var_symbols.keys():
+                    if var_name not in var_symbols:
                         var_symbols[var_name] = 'VAR' + str(var_count)
                         var_count += 1
-                    ascii_line = re.sub(r'\b(' + var_name + r')\b(?:(?=\s*\w+\()|(?!\s*\w+))(?!\s*\()', var_symbols[var_name], ascii_line)
+                    # Replace variable names (not followed by `(`)
+                    ascii_line = re.sub(r'\b(' + var_name + r')\b(?!\s*\()', var_symbols[var_name], ascii_line)
 
             cleaned_gadget.append(ascii_line)
+
     return cleaned_gadget
 
 if __name__ == '__main__':
-    test_gadget = ['function myFunc(var1, var2) {', 'var result = var1 + var2;', 'console.log(result);', '}']
-    test_gadget2 = ['function myFunc(var1, var2) {', 'var result = var1 + var2;', 'console.log(result);', '}', 'function main() {', 'var x = 5;', 'var y = 10;', 'myFunc(x, y);', '}']
-
-    test_gadgetline = ['function(File file, Buffer buff)', 'this is a comment test */']
+    # Example JavaScript gadget to clean
+    test_gadget = [
+        'const fs = require("fs");',
+        'function processFile(file) {',
+        '  let data = fs.readFileSync(file, "utf8");',
+        '  console.log(data);',
+        '}'
+    ]
     
+    test_gadgetline = ['function(File file, Buffer buff)', 'this is a comment test */']
+
     print(clean_gadget(test_gadget))
-    print(clean_gadget(test_gadget2))
     print(clean_gadget(test_gadgetline))
