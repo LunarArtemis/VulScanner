197aa7a55af7af4c6a86dde78ec2104d79f17bed
Lunar <ireena51@gmail.com>
2024-11-11 23:52:58 +0700

    Remove Clean_Gadget2.py and Clean_Gadget3.py; add vectorize_gadgetOG.py for Word2Vec model training and gadget vectorization

--- Clean_Gadget.py
+++ Clean_Gadget.py
@@ -1,19 +1,33 @@
 import re
 
-# JavaScript keywords; immutable set
+# JavaScript keywords; immutable set, REMOVED 'function' and 'const' as they are not considered keywords
 keywords = frozenset({'abstract', 'await', 'boolean', 'break', 'byte', 'case', 'catch', 'char', 'class', 'const',
                       'continue', 'debugger', 'default', 'delete', 'do', 'double', 'else', 'enum', 'export', 'extends',
-                      'false', 'final', 'finally', 'float', 'for', 'function', 'goto', 'if', 'implements', 'import',
+                      'false', 'final', 'finally', 'float', 'for', 'goto', 'if', 'implements', 'import',
                       'in', 'instanceof', 'int', 'interface', 'let', 'long', 'native', 'new', 'null', 'package',
                       'private', 'protected', 'public', 'return', 'short', 'static', 'super', 'switch', 'synchronized',
                       'this', 'throw', 'throws', 'transient', 'true', 'try', 'typeof', 'var', 'void', 'volatile', 'while',
                       'with', 'yield'})
+
 
 # Holds known common function names; immutable set (in place of 'main' in C++)
 main_set = frozenset({'init', 'start'})
 
 # JavaScript/TypeScript doesn’t have argc/argv, but you may add similar terms as needed.
 main_args = frozenset({})
+
+# Compile regex patterns
+rx_comment = re.compile(r'\*/\s*$')
+rx_fun = re.compile(r'\b([_A-Za-z]\w*)\b(?=\s*\()')
+rx_var = re.compile(r'\b([_A-Za-z]\w*)\b(?:(?=\s*\w+\()|(?!\s*\w+))(?!\s*\()')
+rx_str_lit = re.compile(r'"(?:\\.|[^"\\])*"|\'(?:\\.|[^\'\\])*\'|`(?:\\.|[^`\\])*`')
+rx_non_ascii = re.compile(r'[^\x00-\x7f]')
+
+def clean_string_literals_and_non_ascii(line):
+    # Remove all string literals
+    line = rx_str_lit.sub('""', line)
+    # Replace any non-ASCII characters with empty string
+    return rx_non_ascii.sub('', line)
 
 def clean_gadget(gadget):
     fun_symbols = {}
@@ -22,49 +36,63 @@ def clean_gadget(gadget):
     fun_count = 1
     var_count = 1
 
-    # regular expression to catch multi-line comment
-    rx_comment = re.compile(r'\*/\s*$')
-    # regular expression to find function name candidates
-    rx_fun = re.compile(r'\b([_A-Za-z]\w*)\b(?=\s*\()')
-    # regular expression to find variable name candidates
-    rx_var = re.compile(r'\b([_A-Za-z]\w*)\b(?:(?=\s*\w+\()|(?!\s*\w+))(?!\s*\()')
-
     cleaned_gadget = []
 
     for line in gadget:
-        if rx_comment.search(line) is None:
-            # remove all string literals (keep the quotes)
-            nostrlit_line = re.sub(r'"(?:\\.|[^"\\])*"|\'(?:\\.|[^\'\\])*\'|`(?:\\.|[^`\\])*`', '""', line)
-            # replace any non-ASCII characters with empty string
-            ascii_line = re.sub(r'[^\x00-\x7f]', r'', nostrlit_line)
-
-            # return, in order, all regex matches at string list; preserves order for semantics
-            user_fun = rx_fun.findall(ascii_line)
-            user_var = rx_var.findall(ascii_line)
+        if not rx_comment.search(line):
+            line = clean_string_literals_and_non_ascii(line)
+            user_fun = rx_fun.findall(line)
+            user_var = rx_var.findall(line)
 
             for fun_name in user_fun:
-                if len({fun_name}.difference(main_set)) != 0 and len({fun_name}.difference(keywords)) != 0:
-                    if fun_name not in fun_symbols.keys():
-                        fun_symbols[fun_name] = 'FUN' + str(fun_count)
+                if fun_name not in main_set | keywords:
+                    if fun_name not in fun_symbols:
+                        fun_symbols[fun_name] = f'FUN{fun_count}'
                         fun_count += 1
-                    ascii_line = re.sub(r'\b(' + fun_name + r')\b(?=\s*\()', fun_symbols[fun_name], ascii_line)
+                    line = re.sub(rf'\b{fun_name}\b(?=\s*\()', fun_symbols[fun_name], line)
 
             for var_name in user_var:
-                if len({var_name}.difference(keywords)) != 0 and len({var_name}.difference(main_args)) != 0:
-                    if var_name not in var_symbols.keys():
-                        var_symbols[var_name] = 'VAR' + str(var_count)
+                if var_name not in keywords | main_args:
+                    if var_name not in var_symbols:
+                        var_symbols[var_name] = f'VAR{var_count}'
                         var_count += 1
-                    ascii_line = re.sub(r'\b(' + var_name + r')\b(?:(?=\s*\w+\()|(?!\s*\w+))(?!\s*\()', var_symbols[var_name], ascii_line)
+                    line = re.sub(rf'\b{var_name}\b(?:(?=\s*\w+\()|(?!\s*\w+))(?!\s*\()', var_symbols[var_name], line)
 
-            cleaned_gadget.append(ascii_line)
+            cleaned_gadget.append(line)
     return cleaned_gadget
 
 if __name__ == '__main__':
-    test_gadget = ['function myFunc(var1, var2) {', 'var result = var1 + var2;', 'console.log(result);', '}']
-    test_gadget2 = ['function myFunc(var1, var2) {', 'var result = var1 + var2;', 'console.log(result);', '}', 'function main() {', 'var x = 5;', 'var y = 10;', 'myFunc(x, y);', '}']
-
+    test_gadget = [
+        'function myFunc(var1, var2) {',
+        'var result = var1 + var2;',
+        'console.log(result);',
+        '}'
+    ]
+    test_gadget2 = [
+        'function myFunc(var1, var2) {',
+        'var result = var1 + var2;',
+        'console.log(result);',
+        '}',
+        'function main() {',
+        'var x = 5;',
+        'var y = 10;',
+        'myFunc(x, y);',
+        '}'
+    ]
+    test_gadget3 = [
+        "let s = {",
+        "    a: null,",
+        "    b: null,",
+        "    uninit: null",
+        "};",
+        "",
+        "s.a = 20;",
+        "s.b = 20;",
+        "s.uninit = 20;"
+    ]
     test_gadgetline = ['function(File file, Buffer buff)', 'this is a comment test */']
-    
+
     print(clean_gadget(test_gadget))
     print(clean_gadget(test_gadget2))
+    print(clean_gadget(test_gadget3))
     print(clean_gadget(test_gadgetline))
