39c2d06b5bef8dba848253c2ecc3d8b29d98b5df
Lunar <ireena51@gmail.com>
2024-11-12 00:36:55 +0700

    Add VulScan.py for gadget vectorization and model training

---
+++ VulScan.py
@@ -0,0 +1,68 @@
+from vectorize_gadget import GadgetVectorizer
+from Clean_Gadget import clean_gadget
+import pandas
+import sys
+import os
+
+def parse_file(filename):
+    with open(filename, "r", encoding="utf8") as file:
+        gadget = []
+        gadget_val = 0
+        for line in file:
+            stripped = line.strip()
+            if not stripped:
+                continue
+            if "-" * 33 in line and gadget: 
+                yield clean_gadget(gadget), gadget_val
+                gadget = []
+            elif stripped.split()[0].isdigit():
+                if gadget:
+                    # Code line could start with number (somehow)
+                    if stripped.isdigit():
+                        gadget_val = int(stripped)
+                    else:
+                        gadget.append(stripped)
+            else:
+                gadget.append(stripped)
+                
+def get_vectors_df(filename, vector_length=100):
+    gadgets = []
+    count = 0
+    vectorizer = GadgetVectorizer(vector_length)
+    for gadget, val in parse_file(filename):
+        count += 1
+        print("Collecting gadgets...", count, end="\r")
+        vectorizer.add_gadget(gadget)
+        row = {"gadget" : gadget, "val" : val}
+        gadgets.append(row)
+    print('Found {} forward slices and {} backward slices'
+          .format(vectorizer.forward_slices, vectorizer.backward_slices))
+    print()
+    print("Training model...", end="\r")
+    vectorizer.train_model()
+    print()
+    vectors = []
+    count = 0
+    for gadget in gadgets:
+        count += 1
+        print("Processing gadgets...", count, end="\r")
+        vector = vectorizer.vectorize(gadget["gadget"])
+        row = {"vector" : vector, "val" : gadget["val"]}
+        vectors.append(row)
+    print()
+    df = pandas.DataFrame(vectors)
+    return df
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python vuldeepecker.py [filename]")
+        exit()
+    filename = sys.argv[1]
+    base = os.path.splitext(os.path.basename(filename))[0]
+    vector_filename = base + "_gadget_vectors.pkl"
+    vector_length = 50
+    if os.path.exists(vector_filename):
+        df = pandas.read_pickle(vector_filename)
+    else:
+        df = get_vectors_df(filename, vector_length)
+        df.to_pickle(vector_filename)
