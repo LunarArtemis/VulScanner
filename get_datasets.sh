aedd7f51008c01dab6403fbc66acbc342ef8323d
LunarArtemis <ireena51@gmail.com>
2025-03-15 00:34:53 +0700

    Add script to download and extract graph2vec embedding features

---
+++ get_datasets.sh
@@ -0,0 +1,3 @@
+wget -O graph2vec/features/embledding.tar.gz "https://www.dropbox.com/scl/fi/ddgvt54od0fo4li3d0a1d/embledding.tar.gz?rlkey=7qv2bb4d8fz1urk386r8xn631&st=lw070ycx&dl=1"
+tar -xvzf graph2vec/features/embledding.tar.gz -C graph2vec/
+rm graph2vec/features/embledding.tar.gz
